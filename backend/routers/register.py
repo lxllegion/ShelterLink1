9a81ab26021fa3729e628cba18948eea43aff9b8
Charles Zhou <xz265@uw.edu>
2025-10-24 16:44:38 -0700

    add and try mock data

--- backend/routers/register.py
+++ backend/routers/register.py
@@ -3,6 +3,17 @@ from schemas.donor import DonorRegister
 from schemas.shelter import ShelterRegister
 from services.signup import create_donor, create_shelter
 
+from pathlib import Path
+import json
+
+
+DATA_FILE = Path(__file__).resolve().parent.parent / "data" / "mock_data.json"
+
+# mock data
+with open(DATA_FILE, "r") as f:
+    data = json.load(f)
+
+
 # Create a router for registration-related endpoints
 router = APIRouter(
     prefix="/register",
@@ -16,6 +27,9 @@ def register_donor(donor: DonorRegister):
     Endpoint for donor registration.
     Accepts JSON with userID, username, email, and phone_number. for now.
     """
+    data["donors"].append(donor.dict())
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f, indent=4)
     return create_donor(donor)
 
 
@@ -26,4 +40,7 @@ def register_shelter(shelter: ShelterRegister):
     Endpoint for shelter registration.
     Accepts JSON with userID, username, shelter_name, email, and phone_number. for now
     """
+    data["shelters"].append(shelter.dict())
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f, indent=4)
     return create_shelter(shelter)
