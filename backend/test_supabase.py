75ce1a1e84b433b34dbfcd184d67f2e7b6fe395b
lxllegion <xinlei.liu0711@gmail.com>
2025-10-24 17:29:26 -0700

    Supabase connection

---
+++ backend/test_supabase.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine
+# from sqlalchemy.pool import NullPool
+# from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+# load_dotenv()
+
+# Fetch variables
+# USER = os.getenv("user")
+# PASSWORD = os.getenv("password")
+# HOST = os.getenv("host")
+# PORT = os.getenv("port")
+# DBNAME = os.getenv("dbname")
+
+# Construct the SQLAlchemy connection string
+
+# THIS DOES NOT FOR IPv4
+# DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+# THIS ONE WORKS
+DATABASE_URL = f"postgresql://postgres.ukfpqtjwmutklagjssqp:uwcse403@aws-1-us-west-1.pooler.supabase.com:5432/postgres"
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+# If using Transaction Pooler or Session Pooler, we want to ensure we disable SQLAlchemy client side pooling -
+# https://docs.sqlalchemy.org/en/20/core/pooling.html#switching-pool-implementations
+# engine = create_engine(DATABASE_URL, poolclass=NullPool)
+
+# Test the connection
+try:
+    with engine.connect() as connection:
+        print("Connection successful!")
+except Exception as e:
+    print(f"Failed to connect: {e}")
